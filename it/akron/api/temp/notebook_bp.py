f516c272bdf9995842221dc8cefddada7d1cc247
SamuSG11 <samuele.ghezzi1@gmail.com>
2026-06-30 11:30:24 +0200

    route fix

--- it/akron/api/temp/notebook_bp.py
+++ it/akron/api/temp/notebook_bp.py
@@ -9,6 +9,6 @@ def notebook():
     Endpoint che ritorna il link del notebook in colab per addestrare il modello.
     """
     return jsonify({
-        "link-unet: https://colab.research.google.com/drive/1-oHrzo_ILGN4Vn6ZO_NK2vLI-ObPcE7I#scrollTo=SjYICXnBnlGY"
-        "link-efficientnet: https://colab.research.google.com/drive/13ihmNUVQmJwvbS6XNG-4RuaEIGxh9VcD#scrollTo=z-xvSYs2UkEv"
+        "link-unet": "https://colab.research.google.com/drive/1-oHrzo_ILGN4Vn6ZO_NK2vLI-ObPcE7I#scrollTo=SjYICXnBnlGY",
+        "link-efficientnet": "https://colab.research.google.com/drive/13ihmNUVQmJwvbS6XNG-4RuaEIGxh9VcD#scrollTo=z-xvSYs2UkEv"
     }), 200
